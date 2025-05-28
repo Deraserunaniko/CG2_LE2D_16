6318d992789e172c34eba0260534fa322600187e
Deraserunaniko <k024g0076@g.neec.ac.jp>
2025-05-28 16:40:56 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,113 +1,286 @@
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <cstdint>
-#include <format>
-#include <string>
+#include <filesystem>
+#include <fstream>
+#include <chrono>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <cassert>
+#include <DbgHelp.h>
+#include <strsafe.h>
+#include <dxgidebug.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-    WPARAM wparam, LPARAM lparam)
-{
 
-    switch (msg) {
 
-    case WM_DESTROY:
+#pragma comment(lib,"d3d12.lib")
+#pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"Dbghelp.lib")
+#pragma comment(lib,"dxguid.lib")
 
-        PostQuitMessage(0);
-        return 0;
-    }
 
-    return DefWindowProc(hwnd, msg, wparam, lparam);
-}
-
-void Log(const std::string& message)
-{
-
-    OutputDebugStringA(message.c_str());
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception)
+{	
+	return EXCEPTION_EXECUTE_HANDLER;
 }
 
 std::wstring ConvertString(const std::string& str)
 {
-    if (str.empty()) {
-        return std::wstring();
-    }
+	if (str.empty())
+	{
+		return std::wstring();
+	}
 
-    auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
-    if (sizeNeeded == 0) {
-        return std::wstring();
-    }
-    std::wstring result(sizeNeeded, 0);
-    MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
-    return result;
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
 }
 
 std::string ConvertString(const std::wstring& str)
 {
-    if (str.empty()) {
-        return std::string();
-    }
+	if (str.empty())
+	{
+		return std::string();
+	}
 
-    auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
-    if (sizeNeeded == 0) {
-        return std::string();
-    }
-    std::string result(sizeNeeded, 0);
-    WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
-    return result;
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
 }
 
+
+//ウィンドウプロシーシャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	//メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSに対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void Log(std::ostream& os, const std::string& message)
+{
+	os << message << std::endl;
+	OutputDebugStringA(message.c_str());
+}
+
+//Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+	SetUnhandledExceptionFilter(ExportDump);
+	std::filesystem::create_directory("logs");
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+		nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
 
-    Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(),nowSeconds };
 
-    OutputDebugStringA("Hello\n");
+	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
 
-    WNDCLASS wc{};
+	std::string logFilePath = std::string("logs/") + dateString + ".log";
 
-    wc.lpfnWndProc = WindowProc;
+	std::ofstream logStream(logFilePath);
 
-    wc.lpszClassName = L"CG2WindowClass";
+	WNDCLASS wc{};
 
-    wc.hInstance = GetModuleHandle(nullptr);
+	wc.lpfnWndProc = WindowProc;
 
-    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.lpszClassName = L"CG2WindowClass";
 
-    RegisterClass(&wc);
+	wc.hInstance = GetModuleHandle(nullptr);
 
-    const int32_t KClienwidth = 1280;
-    const int32_t KClienHeigt = 720;
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
-    RECT wrc = { 0, 0, KClienwidth, KClienHeigt };
+	RegisterClass(&wc);
 
-    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
 
-    HWND hwnd = CreateWindow(
-        wc.lpszClassName,
-        L"CG2",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT,
-        CW_USEDEFAULT,
-        wrc.right - wrc.left,
-        wrc.bottom - wrc.top,
-        nullptr,
-        nullptr,
-        wc.hInstance,
-        nullptr);
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
-    ShowWindow(hwnd, SW_SHOW);
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-    MSG msg{};
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
 
-    while (msg.message != WM_QUIT) {
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+#ifdef _DEBUG
 
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+	ID3D12Debug1* debugController = nullptr;
 
-            TranslateMessage(&msg);
+	if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+	{
+		//デバッグレイヤーを有効化
+		debugController->EnableDebugLayer();
 
-            DispatchMessage(&msg);
+		//さらにGPU側でもチェックを行うようにする
+		debugController->SetEnableGPUBasedValidation(TRUE);
+	}
+#endif 
+	//DXGIファクトリーの生成
+	IDXGIFactory7* dxgiFactory = nullptr;
 
-        } else {
-        }
-    };
+	//HRESULTはWindows敬のエラーコードであり、関数が成功したかどうかをSUCCEEDEDマクロで判定できる
+	HRESULT hr = CreateDXGIFactory(IID_PPV_ARGS(&dxgiFactory));
 
-    return 0;
+	//初期化の根本的な部分でエラーがでた場合はプログラムが間違っているか、どうにもできない場合が多いのでassertにしておく
+	assert(SUCCEEDED(hr));
+
+	//使用するアダプタ用の変数。最初にnulptrを入れておく
+	IDXGIAdapter4* useAdapter = nullptr;
+
+	//良い淳にアダプタを組む
+	for (UINT i = 0; dxgiFactory->EnumAdapterByGpuPreference(i, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&useAdapter)) != DXGI_ERROR_NOT_FOUND; ++i) {
+		//アダプターの情報を取得する
+		DXGI_ADAPTER_DESC3 adapterDesc{};
+		hr = useAdapter->GetDesc3(&adapterDesc);
+		assert(SUCCEEDED(hr));//取得できないのは一大事
+		//ソフトウェアアダプタでなければ採用！
+		if (!(adapterDesc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE)) {
+			//採用したアダプタの情報をログ出力。wstringのほうなので注意
+			Log(logStream, ConvertString(std::format(L"Use Adapter:{}\n", adapterDesc.Description)));
+			break;
+		}
+		useAdapter = nullptr;
+	}
+	assert(useAdapter != nullptr);
+	ID3D12Device* device = nullptr;
+	D3D_FEATURE_LEVEL featureLevels[] = {
+		D3D_FEATURE_LEVEL_12_2, D3D_FEATURE_LEVEL_12_1, D3D_FEATURE_LEVEL_12_0
+	};
+	const char* featureLevelString[] = { "12.2", "12.1", "12.0" };
+	for (size_t i = 0; i < _countof(featureLevels); ++i)
+	{
+		hr = D3D12CreateDevice(useAdapter, featureLevels[i], IID_PPV_ARGS(&device));
+		if (SUCCEEDED(hr))
+		{
+			Log(logStream, std::format("FeatureLevel : {}\n", featureLevelString[i]));
+			break;
+		}
+	}
+	assert(device != nullptr);
+	Log(logStream, "complete create D3D12Device!!!\n");
+#ifdef _DEBUG
+	ID3D12InfoQueue* infoQueue = nullptr;
+	if (SUCCEEDED(device->QueryInterface(IID_PPV_ARGS(&infoQueue))))
+	{
+		//やばいエラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, true);
+		//エラー時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
+		//警告時に止まる
+		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+		//抑制するメッセージのID
+		D3D12_MESSAGE_ID denyIds[] = { D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE };
+		//windows11でのDXGIデバッグレイヤーとDX12デバッグレイヤーの相互作用バグによるエラーメッセージ
+		//抑制するレベル
+		D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
+		D3D12_INFO_QUEUE_FILTER filter{};
+		filter.DenyList.NumIDs = _countof(denyIds);
+		filter.DenyList.pIDList = denyIds;
+		filter.DenyList.NumSeverities = _countof(severities);
+		//指定したメッセージの表示を抑制する
+		infoQueue->PushStorageFilter(&filter);
+		//解放
+		infoQueue->Release();
+	}
+#endif 
+	ID3D12CommandQueue* commandQueue = nullptr;
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc{};
+	hr = device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&commandQueue));
+	ID3D12CommandAllocator* commandAllocator = nullptr;
+	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
+	assert(SUCCEEDED(hr));
+	ID3D12GraphicsCommandList* commandList = nullptr;
+	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
+	assert(SUCCEEDED(hr));
+	IDXGISwapChain4* swapChain = nullptr;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	swapChainDesc.Width = kClientWidth;
+	swapChainDesc.Height = kClientHeight;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	hr = dxgiFactory->CreateSwapChainForHwnd(commandQueue, hwnd, &swapChainDesc, nullptr, nullptr, reinterpret_cast<IDXGISwapChain1**>(&swapChain));
+	assert(SUCCEEDED(hr));
+	ID3D12DescriptorHeap* rtvDescriptorHeap = nullptr;
+	D3D12_DESCRIPTOR_HEAP_DESC rtvDescriptorHeapDesc{};
+	rtvDescriptorHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvDescriptorHeapDesc.NumDescriptors = 2;
+	hr = device->CreateDescriptorHeap(&rtvDescriptorHeapDesc, IID_PPV_ARGS(&rtvDescriptorHeap));
+	assert(SUCCEEDED(hr));
+	ID3D12Resource* swapChainResources[2] = { nullptr };
+	hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&swapChainResources[0]));
+	assert(SUCCEEDED(hr));
+	hr = swapChain->GetBuffer(1, IID_PPV_ARGS(&swapChainResources[1]));
+	assert(SUCCEEDED(hr));
+	D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+	rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+	rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+	rtvHandles[0] = rtvStartHandle;
+	device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
+	rtvHandles[1].ptr = rtvHandles[0].ptr + device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	device->CreateRenderTargetView(swapChainResources[1], &rtvDesc, rtvHandles[1]);
+	//メインループ
+	MSG msg{};
+	//ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT)
+	{
+		//ウィンドウにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else {
+			UINT backBufferIndex = swapChain->GetCurrentBackBufferIndex();
+			commandList->OMSetRenderTargets(1, &rtvHandles[backBufferIndex], false, nullptr);
+			float clearColor[] = { 0.1f,0.25f,0.5f,1.0f };
+			commandList->ClearRenderTargetView(rtvHandles[backBufferIndex], clearColor, 0, nullptr);
+			hr = commandList->Close();
+			assert(SUCCEEDED(hr));
+			ID3D12CommandList* commandLists[] = { commandList };
+			commandQueue->ExecuteCommandLists(1, commandLists);
+			swapChain->Present(1, 0);
+			hr = commandAllocator->Reset();
+			assert(SUCCEEDED(hr));
+
+			hr = commandList->Reset(commandAllocator, nullptr);
+			assert(SUCCEEDED(hr));
+
+		}
+	}
+	return 0;
 }
