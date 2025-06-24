dfa4c349b53e2cd91026fa806c8253689cc65418
Deraserunaniko <k024g0076@g.neec.ac.jp>
2025-06-24 21:30:36 +0900

    a

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,9 +1,21 @@
-#define float32_t4 float4
+//float4 main( float4 pos : POSITION ) : SV_POSITION
+//{	
+    
+//    return pos;
+//}
+
+//struct TransformationMatrix
+//{
+//    float32_t4x4 WVP;
+//};
+
+
 
 struct VertexShaderOutput
 {
     float32_t4 position : SV_POSITION;
 };
+
 struct VertexShaderInput
 {
     float32_t4 position : POSITION0;
@@ -12,6 +24,7 @@ struct VertexShaderInput
 VertexShaderOutput main(VertexShaderInput input)
 {
     VertexShaderOutput output;
+    
     output.position = input.position;
     return output;
 }
